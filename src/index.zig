3cc25e09be700b1140f9a35ff459f888be0d92a1
Jimmi Holst Christensen <rainbowhejsil@gmail.com>
2017-12-15 22:59:46 +0100

    Added index.zig to src

---
+++ src/index.zig
@@ -0,0 +1,11 @@
+const parser     = @import("parser.zig");
+const functional = @import("functional.zig");
+const comparer   = @import("comparer.zig");
+const algorithm  = @import("algorithm/index.zig");
+
+test "fun-with-zig" {
+    _ = @import("parser.zig");
+    _ = @import("functional.zig");
+    _ = @import("comparer.zig");
+    _ = @import("namespaces/namespaces.zig");
+}
