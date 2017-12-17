e4a304a807d07debd4a97030593d6286e4fa46e2
Jimmi Holst Christensen <rainbowhejsil@gmail.com>
2017-12-17 12:18:49 +0100

    Out commented none committed files for import

--- src/index.zig
+++ src/index.zig
@@ -1,11 +1,12 @@
 const parser     = @import("parser.zig");
 const functional = @import("functional.zig");
 const comparer   = @import("comparer.zig");
-const algorithm  = @import("algorithm/index.zig");
+//const algorithm  = @import("algorithm/index.zig");
 
 test "fun-with-zig" {
     _ = @import("parser.zig");
     _ = @import("functional.zig");
     _ = @import("comparer.zig");
     _ = @import("namespaces/namespaces.zig");
+    //_ = @import("algorithm/index.zig");
 }
