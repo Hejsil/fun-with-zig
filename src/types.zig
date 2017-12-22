1a2fab5cb01731e35c5842db135b597de38fa73d
Jimmi Holst Christensen <jhc@liab.dk>
2017-12-22 14:57:21 +0100

    Added types.zig

---
+++ src/types.zig
@@ -0,0 +1,33 @@
+const TypeId = @import("builtin").TypeId;
+
+/// Determin if ::T can be passed by value.
+pub fn canPassByValue(comptime T: type) -> bool {
+    if (@sizeOf(T) == 0) return true;
+
+    switch (@typeId(T)) {
+        TypeId.Struct,
+        TypeId.Union,
+        TypeId.Array       => return false,
+        TypeId.ErrorUnion,
+        TypeId.Nullable    => return canPassByValue(T.Child),
+        else               => return true
+    }
+}
+
+/// Modify ::T, so it can be passed to a function.
+pub fn Pass(comptime T: type) -> type {
+    if (canPassByValue(T)) {
+        return T;
+    } else {
+        return &const T;
+    }
+}
+
+/// Get the value of ::T. Only useful in generic function that use ::Pass
+pub fn getValue(comptime T: type, value: Pass(T)) -> T {
+    if (canPassByValue(T)) {
+        return value;
+    } else {
+        return *value;
+    }
+}
