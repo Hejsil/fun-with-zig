3733b38b6cf1c781c45a213920a9c8fa3b03fbe7
Jimmi HC <jhc@liab.dk>
2018-05-24 11:30:53 +0200

    Replaced iterator interface example with generic interface

--- fun/interface.zig
+++ fun/interface.zig
@@ -1,84 +1,115 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const debug = std.debug;
+const mem = std.mem;
+const assert = debug.assert;
 
-// TODO: When we have @reify and @reflect, we can just have an Interface type, that takes a comptime
-//       vtable, and generates all the code below
-pub fn Iterator(comptime T: type) type {
+pub const Opaque = @OpaqueType();
+
+pub fn Interface(comptime T: type) type {
+    const info = @typeInfo(T).Struct;
     const VTable = struct {
         const Self = this;
 
-        next: fn([]u8) ?T,
+        funcs: [info.defs.len]fn()void,
 
-        fn init(comptime It: type) Self {
-            return Self {
-                .next = struct {
-                    fn next(d: &u8) ?T {
-                        return It.next(@ptrCast(&T, d));
-                    }
-                }.next
-            };
-        }
-    };
+        fn init(comptime Funcs: type, comptime State: type) Self {
+            var res: Self = undefined;
 
-    return struct {
-        const Self = this;
+            inline for (info.defs) |def, i| {
+                const DefType = @field(T, def.name);
+                comptime assert(@typeOf(DefType) == type);
 
-        data: &u8,
-        vtable: &const VTable,
+                const func_info = @typeInfo(DefType).Fn;
+                comptime assert(func_info.args[0].arg_type == &Opaque);
 
-        pub fn init(data: var) Self {
-            const Type = @typeOf(*data);
-            return Self {
-                .data = @ptrCast(&u8, data),
-                .vtable = &comptime VTable.init(Type),
-            };
-        }
+                const Type = FnType(State, func_info.args[1..], func_info.return_type);
+                const func: Type = @field(Funcs, def.name);
+                res.funcs[i] = @ptrCast(fn()void, func);
+            }
 
-        fn next(it: &const Self) ?T {
-            return it.vtable.next(it.data);
-        }
-    };
-}
-
-pub fn Once(comptime T: type) type {
-    return struct {
-        const Self = this;
-
-        value: ?T,
-
-        pub fn next(it: &Self) ?T {
-            defer it.value = null;
-            const res = it.value;
             return res;
         }
-    };
-}
 
-pub fn Repeat(comptime T: type) type {
+        fn dispatch(vtable: &const Self, comptime fn_name: []const u8, self: &Opaque, args: ...) @field(T, fn_name).ReturnType {
+            inline for (info.defs) |def, i| {
+                if (comptime !mem.eql(u8, def.name, fn_name))
+                    continue;
+
+                const func = @ptrCast(@field(T, def.name), vtable.funcs[i]);
+                return switch (args.len) {
+                    0 => func(self),
+                    1 => func(self, args[0]),
+                    2 => func(self, args[0], args[1]),
+                    3 => func(self, args[0], args[1], args[2]),
+                    else => comptime unreachable,
+                };
+            }
+
+            comptime unreachable;
+        }
+
+        fn FnType(comptime State: type, comptime args: []const builtin.TypeInfo.FnArg, comptime Return: type) type {
+            return switch (args.len) {
+                0 => fn(&State) Return,
+                1 => fn(&State, args[0].arg_type) Return,
+                2 => fn(&State, args[0].arg_type, args[1].arg_type) Return,
+                3 => fn(&State, args[0].arg_type, args[1].arg_type, args[2].arg_type) Return,
+                else => comptime unreachable,
+            };
+        }
+    };
+
     return struct {
         const Self = this;
 
-        value: T,
+        state: &Opaque,
+        vtable: &const VTable,
 
-        pub fn next(it: &Self) ?T {
-            return it.value;
+        pub fn init(comptime State: type, state: &State) Self {
+            return initWithFuncs(State, state, State);
+        }
+
+        pub fn initWithFuncs(comptime State: type, state: &State, comptime Funcs: type) Self {
+            return Self {
+                .state = @ptrCast(&Opaque, state),
+                .vtable = &comptime VTable.init(Funcs, State),
+            };
+        }
+
+        fn call(self: &const Self, comptime fn_name: []const u8, args: ...) @field(T, fn_name).ReturnType {
+            return self.vtable.dispatch(fn_name, self.state, args);
         }
     };
 }
 
-pub fn takeIt(it: &const Iterator(u8)) ?u8 {
-    return it.next();
-}
+const Sb = struct {
+    b: u8,
 
-test "iterator" {
-    var once = Once(u8) { .value = 2 };
-    var repeat = Repeat(u8) { .value = 4 };
-    const once_it = Iterator(u8).init(&once);
-    const repeat_it = Iterator(u8).init(&repeat);
+    fn a(self: &Sb, v: u8) u8 {
+        return self.b + v;
+    }
+};
 
-    @breakpoint();
-    debug.assert(??takeIt(once_it) == 2);
-    debug.assert(  takeIt(once_it) == null);
-    debug.assert(??takeIt(repeat_it) == 2);
-    debug.assert(??takeIt(repeat_it) == 2);
+const Sq = struct {
+    q: u8,
+
+    fn a(self: &Sq, v: u8) u8 {
+        return self.q * v;
+    }
+};
+
+const IA = Interface(
+    struct {
+        const a = fn(&Opaque, u8) u8;
+    }
+);
+
+test "" {
+    var sb = Sb{.b = 3};
+    var sq = Sq{.q = 3};
+    const ib = IA.init(Sb, &sb);
+    const iq = IA.init(Sq, &sq);
+    assert(ib.call("a", u8(2)) == 5);
+    assert(iq.call("a", u8(2)) == 6);
 }
