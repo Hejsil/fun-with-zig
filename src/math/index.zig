d4f4edf837b47b1c5010cc4d145bcc4d1cfd2768
Jimmi Holst Christensen <jimmiholstchristensen@gmail.com>
2018-08-22 21:27:45 +0200

    Fixed digits impl. Credit @DutchGhost

--- src/math/index.zig
+++ src/math/index.zig
@@ -11,8 +11,8 @@ test "math" {
 }
 
 fn digits(comptime N: type, comptime base: comptime_int, n: N) usize {
-    comptime var res = 0;
-    comptime var check = 1;
+    comptime var res = 1;
+    comptime var check = base;
 
     inline while (check <= @maxValue(N)) : ({check *= base; res += 1;}) {
         if (n < check)
@@ -22,13 +22,16 @@ fn digits(comptime N: type, comptime base: comptime_int, n: N) usize {
     return res;
 }
 
-test "digits" {
+test "math.digits" {
+    debug.assert(digits(u1, 10, 0) == 1);
     debug.assert(digits(u1, 10, 1) == 1);
     debug.assert(digits(u2, 10, 3) == 1);
     debug.assert(digits(u8, 10, 255) == 3);
+    debug.assert(digits(u1, 8, 0o0) == 1);
     debug.assert(digits(u1, 8, 0o1) == 1);
     debug.assert(digits(u2, 8, 0o3) == 1);
     debug.assert(digits(u8, 8, 0o255) == 3);
+    debug.assert(digits(u1, 2, 0b0) == 1);
     debug.assert(digits(u1, 2, 0b1) == 1);
     debug.assert(digits(u2, 2, 0b11) == 2);
     debug.assert(digits(u8, 2, 0b1111) == 4);
