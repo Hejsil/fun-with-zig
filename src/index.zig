aa991fc52cd1a630e5d1895b3ae5008b169a3a61
Jimmi Holst Christensen <jhc@liab.dk>
2018-11-06 08:54:42 +0100

    Added bits

--- src/index.zig
+++ src/index.zig
@@ -1,4 +1,5 @@
 pub const ascii = @import("ascii.zig");
+pub const bits = @import("bits.zig");
 pub const generic = @import("generic/index.zig");
 pub const loop = @import("loop.zig");
 pub const math = @import("math/index.zig");
@@ -6,6 +7,7 @@ pub const platform = @import("platform.zig");
 
 test "" {
     _ = ascii;
+    _ = bits;
     _ = generic;
     _ = loop;
     _ = math;
