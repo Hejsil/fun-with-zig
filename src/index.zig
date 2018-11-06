d2e76d6bae27f53ddea72fb8696f553e664a8a66
Jimmi Holst Christensen <jhc@liab.dk>
2018-11-06 08:44:11 +0100

    Added platform.zig

--- src/index.zig
+++ src/index.zig
@@ -1,11 +1,13 @@
 pub const ascii = @import("ascii.zig");
 pub const generic = @import("generic/index.zig");
-pub const loop = @import("loop/index.zig");
+pub const loop = @import("loop.zig");
 pub const math = @import("math/index.zig");
+pub const platform = @import("platform.zig");
 
 test "" {
     _ = ascii;
     _ = generic;
     _ = loop;
     _ = math;
+    _ = platform;
 }
