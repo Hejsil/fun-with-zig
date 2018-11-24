a6d887dff82f896c400850b33f292870a61842e7
Jimmi HC <jimmiholstchristensen@gmail.com>
2018-11-24 20:11:48 +0100

    StringSwitch only asserts for sorted input instead of sorting it itself

--- src/match.zig
+++ src/match.zig
@@ -6,21 +6,17 @@ const rand = std.rand;
 const sort = std.sort;
 
 pub fn StringSwitch(comptime strings: []const []const u8) type {
-    var sorted: [strings.len][]const u8 = undefined;
-    mem.copy([]const u8, sorted[0..], strings);
-    sort.sort([]const u8, sorted[0..], struct {
-        fn lessThan(a: []const u8, b: []const u8) bool {
-            return mem.lessThan(u8, a, b);
-        }
-    }.lessThan);
-    for (sorted[1..]) |_, i|
-        debug.assert(!mem.eql(u8, strings[i], strings[i + 1]));
+    for (strings[1..]) |_, i| {
+        if (mem.lessThan(u8, strings[i], strings[i + 1]))
+            continue;
 
+        @compileError("Input not sorted (assert(\"" ++ strings[i] ++ "\" < \"" ++ strings[i + 1] ++ "\"))");
+    }
 
     return struct {
         pub fn match(str: []const u8) usize {
             var curr: usize = 0;
-            next: for (sorted) |s, i| {
+            next: for (strings) |s, i| {
                 while (curr < s.len) : (curr += 1) {
                     const a = str[curr];
                     const b = s[curr];
@@ -32,12 +28,12 @@ pub fn StringSwitch(comptime strings: []const []const u8) type {
                     return i;
             }
 
-            return sorted.len;
+            return strings.len;
         }
 
         pub fn case(comptime str: []const u8) usize {
             const i = match(str);
-            debug.assert(i < sorted.len);
+            debug.assert(i < strings.len);
             return i;
         }
     };
@@ -45,22 +41,20 @@ pub fn StringSwitch(comptime strings: []const []const u8) type {
 
 test "switch.StringSwitch" {
     @setEvalBranchQuota(1000000);
-    const strings = comptime blk: {
-        var res: [100][]const u8 = undefined;
-        var r = rand.DefaultPrng.init(0);
-
-        for (res) |_, i| {
-            var s: [(i + 2) * 2]u8 = undefined;
-            r.random.bytes(s[0..]);
-            res[i] = s;
-        }
-
-        break :blk res;
+    const strings = [][]const u8{
+        "A",
+        "AA",
+        "AAA",
+        "AAAA",
+        "AAAAA",
+        "AAAAAA",
+        "AAAAAAA",
+        "AAAAAAAA",
     };
-
     const sw = comptime StringSwitch(strings);
 
-    inline for (strings) |str| switch (sw.match(str)) {
+    inline for (strings) |str|
+        switch (sw.match(str)) {
         sw.case(str) => {},
         else => unreachable,
     };
