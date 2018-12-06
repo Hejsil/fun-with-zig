706a5fee467a5fcd1b0f71f42b949b23aeeccd03
Jimmi Holst Christensen <jhc@liab.dk>
2018-12-06 15:38:18 +0100

    I dont like the idea of scanning strings (use another function for that)

--- src/scan.zig
+++ src/scan.zig
@@ -7,7 +7,7 @@ const io = std.io;
 const math = std.math;
 const mem = std.mem;
 
-pub fn scan(allocator: *mem.Allocator, ps: var, comptime fmt: []const u8, comptime Res: type) !Res {
+pub fn scan(ps: var, comptime fmt: []const u8, comptime Res: type) !Res {
     comptime debug.assert(@typeOf(ps.*) == io.PeekStream(1, @typeOf(ps.stream).Error));
     const State = enum {
         Start,
@@ -21,21 +21,6 @@ pub fn scan(allocator: *mem.Allocator, ps: var, comptime fmt: []const u8, compti
     comptime var next_arg = 0;
 
     var res: Res = undefined;
-    if (@sizeOf(Res) != 0)
-        mem.set(u8, mem.asBytes(&res), 0);
-
-    errdefer {
-        skip: inline for (fields) |f| switch (@typeInfo(f.field_type)) {
-            builtin.TypeId.Pointer => |ptr| {
-                if (ptr.size != builtin.TypeInfo.Pointer.Size.Slice)
-                    continue :skip;
-
-                allocator.free(@field(res, f.name));
-            },
-            else => {},
-        };
-    }
-
     inline for (fmt) |c, i| {
         switch (state) {
             State.Start => switch (c) {
@@ -56,7 +41,7 @@ pub fn scan(allocator: *mem.Allocator, ps: var, comptime fmt: []const u8, compti
                 '}' => {
                     const field = fields[next_arg];
                     const inner_fmt = fmt[start_index..i];
-                    @field(res, field.name) = try scanOne(allocator, ps, inner_fmt, field.field_type);
+                    @field(res, field.name) = try scanOne(ps, inner_fmt, field.field_type);
                     next_arg += 1;
                     state = State.Start;
                 },
@@ -89,23 +74,11 @@ fn expect(ps: var, char: u8) !void {
         return error.InvalidCharacter;
 }
 
-fn scanOne(allocator: *mem.Allocator, ps: var, comptime fmt: []const u8, comptime T: type) !T {
+fn scanOne(ps: var, comptime fmt: []const u8, comptime T: type) !T {
     switch (@typeInfo(T)) {
         builtin.TypeId.Int => return try scanInt(ps, fmt, T),
         builtin.TypeId.Float => return try scanFloat(ps, fmt, T),
         builtin.TypeId.Bool => return try scanBool(ps, fmt),
-        builtin.TypeId.Pointer => |ptr| {
-            err: {
-                if (ptr.size != builtin.TypeInfo.Pointer.Size.Slice)
-                    break :err;
-                if (ptr.child != u8)
-                    break :err;
-
-                return try scanString(allocator, ps, fmt);
-            }
-
-            @compileError("Unable to format type '" ++ @typeName(T) ++ "'");
-        },
         else => @compileError("Unable to format type '" ++ @typeName(T) ++ "'"),
     }
 }
@@ -113,7 +86,7 @@ fn scanOne(allocator: *mem.Allocator, ps: var, comptime fmt: []const u8, comptim
 fn scanInt(ps: var, comptime fmt: []const u8, comptime Int: type) !Int {
     const base: u8 = if (fmt.len == 0)
         u8(10)
-    else switch(fmt[0]) {
+    else switch (fmt[0]) {
         'b' => u8(2),
         'd' => u8(10),
         'x' => u8(16),
@@ -151,6 +124,20 @@ fn scanIntRest(ps: var, op_first: ?u8, comptime Int: type, base: u8) !Int {
     }
 }
 
+pub fn charToDigit(c: u8, base: u8) (error{InvalidCharacter}!u8) {
+    const value = switch (c) {
+        '0'...'9' => c - '0',
+        'A'...'Z' => c - 'A' + 10,
+        'a'...'z' => c - 'a' + 10,
+        else => return error.InvalidCharacter,
+    };
+
+    if (value >= base)
+        return error.InvalidCharacter;
+
+    return value;
+}
+
 fn scanFloat(ps: var, comptime fmt: []const u8, comptime Float: type) !Float {
     @compileError("TODO");
 }
@@ -172,122 +159,11 @@ fn scanBool(ps: var, comptime fmt: []const u8) !bool {
     return str[0] == 't';
 }
 
-fn scanString(allocator: *mem.Allocator, ps: var, comptime fmt: []const u8) ![]u8 {
-    const valid_chars = comptime blk: {
-        const State = union(enum) {
-            Begin,
-            Char: u8,
-            Escape,
-            Range: u8,
-        };
-
-        var state: State = State.Begin;
-        var res = []bool{false} ** (math.maxInt(u8) + 1);
-        for (fmt) |c| switch (state) {
-            State.Begin => switch (c) {
-                '\\' => state = State.Escape,
-                '-' => @compileError("TODO: Error"),
-                else => state = State{.Char = c},
-            },
-            State.Char => |last| switch (c) {
-                '\\' => {
-                    res[last] = true;
-                    state = State.Escape;
-                },
-                '-' => state = State{.Range = last},
-                else => {
-                    res[last] = true;
-                    state = State{.Char = c};
-                },
-            },
-            State.Escape => |last| switch (c) {
-                '\\', '-' => state = State{.Char = c},
-                else => @compileError("TODO: Error"),
-            },
-            State.Range => |first| switch (c) {
-                '-' => @compileError("TODO: Error"),
-                else => {
-                    if (c < first)
-                        @compileError("TODO: Error");
-
-                    var i = first;
-                    while (i < c) : (i += 1)
-                        res[i] = true;
-
-                    state = State.Begin;
-                },
-            },
-        };
-
-        switch (state) {
-            State.Begin => {},
-            State.Char => |last| res[last] = true,
-            State.Escape => @compileError("TODO: Error"),
-            State.Range => @compileError("TODO: Error"),
-        }
-
-        break :blk res;
-    };
-
-    return try scanValidChars(allocator, ps, valid_chars);
-}
-
-fn scanValidChars(allocator: *mem.Allocator, ps: var, valid: [math.maxInt(u8) + 1]bool) ![]u8 {
-    var buf = try std.Buffer.initSize(allocator, 0);
-    errdefer buf.deinit();
-
-    while (true) {
-        const byte = ps.stream.readByte() catch |err| switch (err) {
-            error.EndOfStream => return buf.toOwnedSlice(),
-            else => return err,
-        };
-
-        if (!valid[byte]) {
-            ps.putBackByte(byte);
-            return buf.toOwnedSlice();
-        }
-
-        try buf.appendByte(byte);
-    }
-}
-
-pub fn charToDigit(c: u8, base: u8) (error{InvalidCharacter}!u8) {
-    const value = switch (c) {
-        '0'...'9' => c - '0',
-        'A'...'Z' => c - 'A' + 10,
-        'a'...'z' => c - 'a' + 10,
-        else => return error.InvalidCharacter,
-    };
-
-    if (value >= base)
-        return error.InvalidCharacter;
-
-    return value;
-}
-
-test "scan.NoArgs" {
-    var buf: [2 * 1024]u8 = undefined;
-    var fix_buf_alloc = heap.FixedBufferAllocator.init(buf[0..]);
-    const allocator = &fix_buf_alloc.allocator;
-
-    const string = "abcd";
-    inline for (string) |_, i| {
-        var mem_stream = io.SliceInStream.init(string);
-        var ps = io.PeekStream(1, io.SliceInStream.Error).init(&mem_stream.stream);
-
-        _ = try scan(allocator, &ps, string[0 .. i + 1], struct {});
-    }
-}
-
 fn testScanOk(comptime fmt: []const u8, str: []const u8, comptime T: type, res: T) !void {
-    var buf: [2 * 1024]u8 = undefined;
-    var fix_buf_alloc = heap.FixedBufferAllocator.init(buf[0..]);
-    const allocator = &fix_buf_alloc.allocator;
-
     var mem_stream = io.SliceInStream.init(str);
     var ps = io.PeekStream(1, io.SliceInStream.Error).init(&mem_stream.stream);
 
-    const result = try scan(allocator, &ps, fmt, struct {
+    const result = try scan(&ps, fmt, struct {
         r: @typeOf(res),
     });
     switch (T) {
@@ -297,14 +173,10 @@ fn testScanOk(comptime fmt: []const u8, str: []const u8, comptime T: type, res: 
 }
 
 fn testScanError(comptime fmt: []const u8, str: []const u8, comptime T: type, err: anyerror) void {
-    var buf: [2 * 1024]u8 = undefined;
-    var fix_buf_alloc = heap.FixedBufferAllocator.init(buf[0..]);
-    const allocator = &fix_buf_alloc.allocator;
-
     var mem_stream = io.SliceInStream.init(str);
     var ps = io.PeekStream(1, io.SliceInStream.Error).init(&mem_stream.stream);
 
-    debug.assertError(scan(allocator, &ps, fmt, struct {
+    debug.assertError(scan(&ps, fmt, struct {
         i: T,
     }), err);
 }
@@ -363,16 +235,4 @@ test "scanBool" {
     testScanError("{}", "qalse", bool, error.InvalidCharacter);
     testScanError("{}", "frue", bool, error.InvalidCharacter);
     testScanError("{}", "talse", bool, error.InvalidCharacter);
-}
-
-test "scanString" {
-    try testScanOk("{a}", "", []const u8, "");
-    try testScanOk("{a}", "a", []const u8, "a");
-    try testScanOk("{a}", "aa", []const u8, "aa");
-    try testScanOk("{a-z}", "abcd", []const u8, "abcd");
-    try testScanOk("a{a-z}", "abcd", []const u8, "bcd");
-    try testScanOk("a{a-z}Z", "abcdZ", []const u8, "bcd");
-    //try testScanOk("a{\\\\}c", "a-c", []const u8, "-");
-
-    testScanError("a{}b", "acb", bool, error.InvalidCharacter);
 }
