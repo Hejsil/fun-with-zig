a292dc335c4792a78dfe3eee2e8b93bed30dbcad
Jimmi Holst Christensen <jhc@liab.dk>
2019-02-04 09:52:26 +0100

    Moved benchmarks to own folder

---
+++ bench/scan.zig
@@ -0,0 +1,159 @@
+const std = @import("std");
+const bench = @import("bench");
+const fun = @import("fun-with-zig");
+
+const io = std.io;
+
+const scan = fun.scan.scan;
+const sscan = fun.scan.sscan;
+
+fn sscanSlow(str: []const u8, comptime fmt: []const u8, comptime Res: type) !Res {
+    var mem_stream = io.SliceInStream.init(str);
+    var ps = io.PeekStream(1, io.SliceInStream.Error).init(&mem_stream.stream);
+    return try scan(&ps, fmt, Res);
+}
+
+test "scan.sscan.benchmark.single" {
+    try bench.benchmark(struct {
+        const args = [][]const u8{
+            "0=0",
+            "10=10",
+            "210=210",
+            "3210=3210",
+            "43210=43210",
+            "543210=543210",
+            "6543210=6543210",
+            "76543210=76543210",
+            "876543210=876543210",
+        };
+
+        fn scanSingle(str: []const u8) !u64 {
+            const res = try sscanSlow(str, "{}={}", struct {
+                a: u32,
+                b: u32,
+            });
+            return u64(res.a) + res.b;
+        }
+
+        fn sscanSingle(str: []const u8) !u64 {
+            const res = try sscan(str, "{}={}", struct {
+                a: u32,
+                b: u32,
+            });
+            return u64(res.a) + res.b;
+        }
+    });
+}
+
+test "scan.sscan.benchmark.switch" {
+    try bench.benchmark(struct {
+        const args = [][]const u8{
+            "foo=0",
+            "foo.bar=0",
+            "foo.bar.baz=0",
+            "foo[0].bar[0].baz[0]=0",
+            "baz=0",
+            "baz.bar=0",
+            "baz.bar.foo=0",
+            "baz[0].bar[0].foo[0]=0",
+            "foo=9223372036854775807",
+            "foo.bar=9223372036854775807",
+            "foo.bar.baz=9223372036854775807",
+            "foo[9223372036854775807].bar[9223372036854775807].baz[9223372036854775807]=9223372036854775807",
+            "baz=9223372036854775807",
+            "baz.bar=9223372036854775807",
+            "baz.bar.foo=9223372036854775807",
+            "baz[9223372036854775807].bar[9223372036854775807].foo[9223372036854775807]=9223372036854775807",
+        };
+
+        const iterations = 100000;
+
+        fn scanSwitch(str: []const u8) u128 {
+            if (sscanSlow(str, "foo={}", struct {
+                a: u64,
+            })) |v| {
+                return v.a;
+            } else |_| if (sscanSlow(str, "foo.bar={}", struct {
+                a: u64,
+            })) |v| {
+                return v.a;
+            } else |_| if (sscanSlow(str, "foo.bar.baz={}", struct {
+                a: u64,
+            })) |v| {
+                return v.a;
+            } else |_| if (sscanSlow(str, "foo[{}].bar[{}].baz[{}]={}", struct {
+                a: u64,
+                b: u64,
+                c: u64,
+                d: u64,
+            })) |v| {
+                return u128(v.a) + v.b + v.c + v.d;
+            } else |_| if (sscanSlow(str, "baz={}", struct {
+                a: u64,
+            })) |v| {
+                return v.a;
+            } else |_| if (sscanSlow(str, "baz.bar={}", struct {
+                a: u64,
+            })) |v| {
+                return v.a;
+            } else |_| if (sscanSlow(str, "baz.bar.foo={}", struct {
+                a: u64,
+            })) |v| {
+                return v.a;
+            } else |_| if (sscanSlow(str, "baz[{}].bar[{}].foo[{}]={}", struct {
+                a: u64,
+                b: u64,
+                c: u64,
+                d: u64,
+            })) |v| {
+                return u128(v.a) + v.b + v.c + v.d;
+            } else |_| {
+                unreachable;
+            }
+        }
+
+        fn sscanSwitch(str: []const u8) u128 {
+            if (sscan(str, "foo={}", struct {
+                a: u64,
+            })) |v| {
+                return v.a;
+            } else |_| if (sscan(str, "foo.bar={}", struct {
+                a: u64,
+            })) |v| {
+                return v.a;
+            } else |_| if (sscan(str, "foo.bar.baz={}", struct {
+                a: u64,
+            })) |v| {
+                return v.a;
+            } else |_| if (sscan(str, "foo[{}].bar[{}].baz[{}]={}", struct {
+                a: u64,
+                b: u64,
+                c: u64,
+                d: u64,
+            })) |v| {
+                return u128(v.a) + v.b + v.c + v.d;
+            } else |_| if (sscan(str, "baz={}", struct {
+                a: u64,
+            })) |v| {
+                return v.a;
+            } else |_| if (sscan(str, "baz.bar={}", struct {
+                a: u64,
+            })) |v| {
+                return v.a;
+            } else |_| if (sscan(str, "baz.bar.foo={}", struct {
+                a: u64,
+            })) |v| {
+                return v.a;
+            } else |_| if (sscan(str, "baz[{}].bar[{}].foo[{}]={}", struct {
+                a: u64,
+                b: u64,
+                c: u64,
+                d: u64,
+            })) |v| {
+                return u128(v.a) + v.b + v.c + v.d;
+            } else |_| {
+                unreachable;
+            }
+        }
+    });
+}
--- build.zig
+++ build.zig
@@ -6,20 +6,32 @@ const Mode = builtin.Mode;
 
 pub fn build(b: *Builder) void {
     const test_all_step = b.step("test", "Run all tests in all modes.");
+    const bench_all_step = b.step("benchmark", "Run all benchmarks in all modes.");
     inline for ([]Mode{ Mode.Debug, Mode.ReleaseFast, Mode.ReleaseSafe, Mode.ReleaseSmall }) |test_mode| {
         const mode_str = comptime modeToString(test_mode);
 
         const t = b.addTest("index.zig");
-        t.addPackagePath("bench", "lib/zig-bench/src/index.zig");
         t.setBuildMode(test_mode);
         t.setNamePrefix(mode_str ++ " ");
 
         const test_step = b.step("test-" ++ mode_str, "Run all tests in " ++ mode_str ++ ".");
         test_step.dependOn(&t.step);
         test_all_step.dependOn(test_step);
+
+
+        const bench = b.addTest("bench/index.zig");        
+        bench.addPackagePath("bench", "lib/zig-bench/src/index.zig");
+        bench.addPackagePath("fun-with-zig", "index.zig");
+        bench.setBuildMode(test_mode);
+        bench.setNamePrefix(mode_str ++ " ");
+
+        const bench_step = b.step("bench-" ++ mode_str, "Run all benchmarks in " ++ mode_str ++ ".");
+        bench_step.dependOn(&bench.step);
+        bench_all_step.dependOn(bench_step);
     }
 
     b.default_step.dependOn(test_all_step);
+    b.default_step.dependOn(bench_all_step);
 }
 
 fn modeToString(mode: Mode) []const u8 {
